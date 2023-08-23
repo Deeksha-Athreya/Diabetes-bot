826fd6ba71e22e87f95c960387aeecc04748a001
Deeksha <117797100+Deeksha-Athreya@users.noreply.github.com>
2023-08-23 22:11:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,37 @@
 import streamlit as st
 import pandas as pd
-import pickle
-from sklearn.preprocessing import StandardScaler
-import sys
-sys.path.append('/path/to/sklearn')
+import joblib
 
+def main():
+    st.title('Diabetes Prediction')
+    
+   
+    pregnancies = st.slider('Number of Pregnancies', 0, 17, 0)
+    age = st.slider('Age', 1, 100, 25)
+    bmi = st.slider('BMI', 10.0, 60.0, 25.0)
+    
+   
+    model = joblib.load('model.joblib')
+    
+    # Predict
+    if st.button('Predict'):
+        
+        input_data = pd.DataFrame({
+            'Pregnancies': [pregnancies],
+            'Age': [age],
+            'BMI': [bmi]
+           
+        })
+        
+        
+        prediction = model.predict(input_data)
+        
+        
+        if prediction[0] == 1:
+            st.error('The person is predicted to have diabetes.')
+        else:
+            st.success('The person is predicted to be diabetes-free.')
 
-with open('Diabetes_prediction.ipynb', 'rb') as file:
-    model = pickle.load(file)
+if __name__ == '__main__':
+    main()
 
-
-st.title('Diabetes Prediction App')
-
-
-uploaded_file = st.file_uploader("Choose a file")
-
-
-if uploaded_file is not None:
-    data = pd.read_csv(uploaded_file)
-    scaler = StandardScaler()
-    data = scaler.fit_transform(data)
-    predictions = model.predict(data)
-    st.write(predictions)
