da1fefe03e116f37b2f1eabfe2f81882438b2594
Deeksha <117797100+Deeksha-Athreya@users.noreply.github.com>
2023-08-23 22:54:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,78 @@ import pickle
 
 import pickle
 
-# Extracted content of the IPython Notebook file
-Diabetes_prediction= {
-    "metadata": {...},
-    "nbformat": ...,
-    "nbformat_minor": ...,
-    "cells": [...]
-}
+# Train your model
+model =
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+%matplotlib inline
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score
+from matplotlib.colors import ListedColormap
 
-# Save the content as a pickle file
-with open('bot.pkl', 'wb') as file:
-    pickle.dump(Diabetes_prediction, file)
+dataset = pd.read_csv('diabetes.csv')
+dataset.shape
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+
+
+corrmat = dataset.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(20,20))
+
+g=sns.heatmap(dataset[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+
+
+
+dataset.corr()
+
+# check if any null value is present
+dataset.isnull().values.any()
+
+
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+print(X)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+classifier = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 0)
+classifier.fit(X_train, y_train)
+random_forest_model = RandomForestClassifier(random_state=10)
+
+random_forest_model.fit(X_train, y_train.ravel())
+(predict_train_data := random_forest_model.predict(X_test))
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
+inputdata1=np.asarray(inputdata)
+inputdata2=inputdata1.reshape(1,-1)
+predict=classifier.predict(inputdata2)
+
+# Save the trained model as a pickle file
+with open('model.pkl', 'wb') as file:
+    pickle.dump(model, file)
+
+
 
 def main():
     st.title('Diabetes Prediction')
