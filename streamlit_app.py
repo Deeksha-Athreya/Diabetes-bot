22635029730c22d58e4ff792d1354f9f6c234981
Deeksha <117797100+Deeksha-Athreya@users.noreply.github.com>
2023-08-23 19:30:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,25 @@
 import streamlit as st
 import pandas as pd
+import pickle
+from sklearn.preprocessing import StandardScaler
 
-st.title('Diabetes prediction bot')
+
+with open('diabetes_model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+
+st.title('Diabetes Prediction App')
+
 
 uploaded_file = st.file_uploader("Choose a file")
 
+
 if uploaded_file is not None:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    data = pd.read_csv(uploaded_file)
+    scaler = StandardScaler()
+    data = scaler.fit_transform(data)
+    predictions = model.predict(data)
+    st.write(predictions)
 
 
 
