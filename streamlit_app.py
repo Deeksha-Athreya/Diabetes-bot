e98a084f8396d3a6b455631c1fb39d291ab3ec5b
Deeksha <117797100+Deeksha-Athreya@users.noreply.github.com>
2023-08-23 23:06:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,6 @@
 import streamlit as st
 import pandas as pd
 import pickle
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score
-
-# Load the dataset
-dataset = pd.read_csv('diabetes.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
-# Handle missing values using SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-
-# Split dataset into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
-
-# Standardize features
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-# Train the Random Forest model
-classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
-classifier.fit(X_train, y_train)
-
-# Load the trained model (you don't need to load the notebook)
-# Use 'model.pkl' instead of 'Diabetes_prediction.ipynb'
-with open('model.pkl', 'rb') as file:
-    model = pickle.load(file)
 
 def main():
     st.title('Diabetes Prediction')
