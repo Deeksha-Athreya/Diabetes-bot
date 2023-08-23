f4b388cbc7a48f1051b92fb0a9ac23a2ca9ce340
Deeksha <117797100+Deeksha-Athreya@users.noreply.github.com>
2023-08-24 00:16:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,38 +8,35 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 import streamlit as st
 
-# Load the dataset
+
 dataset = pd.read_csv('diabetes.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# Handle missing values using SimpleImputer
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-# Split dataset into train and test sets
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-# Standardize features
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# Train the Random Forest model
+
 classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
 classifier.fit(X_train, y_train)
 
-# Evaluate the model
 y_pred = classifier.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Accuracy: {accuracy}')
 
-# Save the trained model as a pickle file
+
 with open('model.pkl', 'wb') as file:
     pickle.dump(classifier, file)
 
-# Streamlit app
+
 def main():
     st.title('Diabetes Prediction')
 
@@ -67,10 +64,10 @@ def main():
         'BMI': [bmi]
     })
 
-    # Make the prediction
+ 
     prediction = model.predict(input_data)
 
-    # Show the prediction result
+  
     if prediction[0] == 1:
         st.error('The person is predicted to have diabetes.')
     else:
