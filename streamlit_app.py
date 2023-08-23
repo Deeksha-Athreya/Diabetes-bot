837836f4f501662e8e31b36957d7be5e948097c4
Deeksha <117797100+Deeksha-Athreya@users.noreply.github.com>
2023-08-23 22:58:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-%matplotlib inline
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.impute import SimpleImputer
