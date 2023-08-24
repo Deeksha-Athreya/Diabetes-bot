651277bca7bf676c3d4644c8981f6bd9f356c4ee
Deeksha <117797100+Deeksha-Athreya@users.noreply.github.com>
2023-08-24 09:39:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,47 +1,16 @@
 import numpy as np
 import pandas as pd
 import pickle
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
 import streamlit as st
 
 
-dataset = pd.read_csv('diabetes.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
-
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-
-classifier = RandomForestClassifier(n_estimators=10, criterion='entropy', random_state=0)
-classifier.fit(X_train, y_train)
-
-y_pred = classifier.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Accuracy: {accuracy}')
-
-
-with open('model.pkl', 'wb') as file:
-    pickle.dump(classifier, file)
 
 
 def main():
     st.title('Diabetes Prediction')
 
     # Load the trained model
-    with open('model.pkl', 'rb') as file:
+    with open('classifier.pkl', 'rb') as file:
         model = pickle.load(file)
 
     # Input form
